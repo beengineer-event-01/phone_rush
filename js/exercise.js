756784621c48273062288f8f571fc05189fd652a
Shotaro <shotaro@saekimiyokonoMacBook-Air-2.local>
2024-12-27 14:50:46 +0900

    センサの値を取得できるようにした

--- js/exercise.js
+++ js/exercise.js
@@ -1,6 +1,25 @@
+console.log(1 + 2)
+
+ let num = 5;
+ num = 10;
+console.log(num);
+
 function sumFilteredEvens(numbers) {
   // 演習問題 1
   // ここにコードを書いてください
+  let sum = 0
+  // for(let i = 0; i < 8; length(numbers)++){}
+  for(let i = 0; i < 8; i++){
+    if (numbers[i] % 2 == 0 && numbers[i] % 5 != 0){
+      sum += numbers[i];
+    }
+  }
+  numbers.foreach((number) => {
+    if(number % 2 == 0){
+      sum += number
+    }
+  })
+  return sum;
 }
 
 const numbers = [2, 4, 5, 10, 12, 15, 20, 22];
@@ -13,3 +32,16 @@ function isPrime(number) {
 
 number = 5;
 console.log(isPrime(number));
+
+console.log(numbers);
+numbers.pop();
+console.log(numbers);
+
+ numbers.shift();
+console.log(numbers);
+
+numbers.push(100, 200);
+console.log(numbers);
+
+numbers.unshift(-1, -2);
+console.log(numbers);
--- js/main.js
+++ js/main.js
@@ -109,6 +109,29 @@ const glbloader = new GLTFLoader();
 
 // プレイヤーの描画
 // ここに記述
+glbloader.load(
+  glbUrls[0],
+  function (gltf) {
+    player =gltf.scene;
+    player.scale.set(3, 2, 3);
+    player.rotation.set(0, Math.PI, 0);
+    player.position.set(0, 0, 0);
+    mixer = new AnimationMixer(player);
+    const runningAction = gltf.animations.find(
+      (animation) => animation.name === "running"
+    );
+    if(runningAction) {
+      mixer.clipAction(runningAction).play();
+    } else {
+      console.warn("Running animation not found in the model.");
+    }
+    scene.add(player);
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+)
 
 // 建物の描画
 glbloader.load(
@@ -131,6 +154,24 @@ glbloader.load(
 
 // スマホの描画
 // ここに記述
+glbloader.load(
+  glbUrls[2],
+  function (gltf) {
+    for (let g = 1; g < 10; g++) {
+      model = gltf.scene.clone();
+      model.scale.set(15, 15, 15);
+      model.rotation.set(0, Math.PI / 4, Math.PI / 4);
+      const randomIndex = Math.floor(Math.random() * 3); // 0 、1 、2 のランダム
+      model.position.set(course[randomIndex], 2, -10 * g);
+      phone_list.push(model); // オブジェクトのバウンディングボックスを計算
+      scene.add(model);
+    }
+  },
+  undefined,
+  function (error) {
+    console.error(error);
+  }
+);
 
 // 障害物の描画
 for (let g = 1; g < 12; g++) {
@@ -163,6 +204,22 @@ textureloader.load(
 
 // ゴールの描画
 // ここに記述
+textureloader.load(
+  textureUrls[1],
+  function (texture){
+    geometry = new BoxGeometry(24, 10, 0.5);
+    sphereMaterial = new MeshPhongMaterial();
+    sphereMaterial.map = texture;
+    goal = new Mesh(geometry, sphereMaterial);
+    goal.position.set(0, 5, -200);
+    goalBoundingBox = new Box3().setFromObject(goal);
+    scene.add(goal);
+  },
+  undefined,
+  function (error) {
+    console.errpr(error);
+  }
+);
 
 // センサの値の読み取り
 document.addEventListener("DOMContentLoaded", function () {
@@ -195,6 +252,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
   // ジャイロセンサの値の取得
   // ここに追加
+  window.addEventListener(
+    "deviceorientation",
+    (event) => {
+      alpha = event.alpha || 0;
+      beta = event.beta || 0;
+      gamma = event.gamma || 0;
+      console.log("Gyro:", alpha, beta, gamma);
+    },
+    false
+  );
+
 
   // 一定時間ごとに
   let graphtimer = window.setInterval(() => {
@@ -229,6 +297,7 @@ document.addEventListener("DOMContentLoaded", function () {
 // プレイヤーの移動
 function move() {
   // ここに追加
+  player.position.z -= 0.2;
 }
 
 // プレイヤーのジャンプ
@@ -275,16 +344,22 @@ function animate() {
 
   // Mixer
   // ここに追加
+if (mixer) {
+  mixer.update(0.9);
+}
 
   if (player) {
     // 移動関数の実行
     // ここに追加
+    move();
     // ジャンプ関数の実行
     // ここに追加
     // 衝突判定関数の実行
     // ここに追加
     // カメラの移動
     // ここに追加
+    camera.position.set(0, 8, player.position.z + 10);
+    camera.lookAt(new Vector3(0, 5, player.position.z));
   }
   renderer.render(scene, camera);
 }
